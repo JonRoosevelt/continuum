4fdba6bc47a8ef19afea533db07a31708e3cf48f
Jonathan Martins <jon@jonmartins.dev>
2026-09-25 12:42:20 +0100

    chore(release): v0.1.1

--- packaging/continuum.rb
+++ packaging/continuum.rb
@@ -1,6 +1,6 @@
 # Installs a prebuilt app; requires a signed, notarized release artifact.
 cask "continuum" do
-  version "0.1.0"
+  version "0.1.1"
   sha256 :no_check
 
   url "https://github.com/JonRoosevelt/continuum/releases/download/v#{version}/Continuum-#{version}.zip"
